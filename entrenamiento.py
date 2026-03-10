1bace6ab0715825f0dd7333077394d72d346c598
RayanSiffeO <icebynexus07@gmail.com>
2026-03-10 21:04:08 +0100

    Update entrenamiento.py

--- entrenamiento.py
+++ entrenamiento.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 #Lectura y manipulacion de los datos
-df_review = pd.read_csv("C:/Users/RAYANs/Documents/Scripts/Excel/IMDB Dataset.csv")
+df_review = pd.read_csv("Excel/IMDB Dataset.csv")
 #Input(x) - > Comentarios(review)
 #Output(y) - > Sentimientos
 redux = RandomUnderSampler()
@@ -39,6 +39,7 @@ pc_dtm = pd.DataFrame(cv_matrix.toarray(), index = pc['review'].values , columns
 tf = TfidfVectorizer(stop_words='english')
 tf_matrix = tf.fit_transform(pc['text'])
 tf_dtm = pd.DataFrame(tf_matrix.toarray(), index = pc['review'].values , columns = tf.get_feature_names_out() )
+# Nota: Las palabras clave por peso son mas precisas que por cantidad (da importancia a palabras de relleno.)
 
 
 #Transformacion de data string a integers
@@ -61,7 +62,7 @@ mnb = MultinomialNB()
 mnb.fit(entr_vectorx, entr_y)
 # Regresion logistica
 from sklearn.linear_model import LogisticRegression
-lr = LogisticRegression()
+lr = LogisticRegression(max_iter=1000)
 lr.fit(entr_vectorx, entr_y)
 
 #Evaluacion de modelos
@@ -132,5 +133,6 @@ print(f"Mejor score obtenido en validación: {svc_grid.best_score_:.4f}")
 import joblib
 joblib.dump(lr, "models/modelo_sentimientos.pkl")
 joblib.dump(tf, "models/vectorizador.pkl")
+
 
 
